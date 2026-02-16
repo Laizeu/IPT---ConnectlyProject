07102f14247553f7010ab904b893614b4cf7dbc1
Laiza Veronica Llanto <laizallanto.academic@gmail.com>
2026-02-17 06:19:12 +0800

    Final working API version

--- posts/models.py
+++ posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Define the Post model - L.Llanto
+# Define the models - L.Llanto
 
 class User(models.Model):
     username = models.CharField(max_length=100, unique=True)  # User's unique username
